7e51a2f14bca47f59cc0b84c1462d5fcad4cb795
theDude1101 <120323536+theDude1101@users.noreply.github.com>
2023-11-30 20:44:40 +0300

    stage 1

---
+++ js/script-bio.js
@@ -0,0 +1,15 @@
+$('.menu-button').on('click', function(e) {
+  e.preventDefault();
+  $('.menu-block').toggleClass('menu_active');
+  $('.header-block').toggleClass('header-block_active');
+  $('.text-block').toggleClass('text-block_active');
+	$('.main-text').toggleClass('main-text_active');
+})
+
+$('.close-button').on('click', function(e) {
+  e.preventDefault();
+  $('.menu-block').removeClass('menu_active');
+  $('.header-block').removeClass('header-block_active');
+  $('.text-block').removeClass('text-block_active');
+	$('.main-text').removeClass('main-text_active');
+})
---
+++ js/script.js
@@ -0,0 +1,13 @@
+$('.menu-button').on('click', function(e) {
+  e.preventDefault();
+  $('.menu-block').toggleClass('menu_active');
+  $('.header-block').toggleClass('header-block_active');
+  $('.text-block').toggleClass('text-block_active');
+})
+
+$('.close-button').on('click', function(e) {
+  e.preventDefault();
+  $('.menu-block').removeClass('menu_active');
+  $('.header-block').removeClass('header-block_active');
+  $('.text-block').removeClass('text-block_active');
+})
