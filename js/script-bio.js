12b10c2d2f6695ecbc7f7f9b96f26a2f63f5b245
thedude1100 <120323536+theDude1101@users.noreply.github.com>
2024-03-21 19:05:31 +0300

    Update script-bio.js

--- js/script-bio.js
+++ js/script-bio.js
@@ -13,3 +13,19 @@ $('.close-button').on('click', function(e) {
   $('.text-block').removeClass('text-block_active');
 	$('.main-text').removeClass('main-text_active');
 })
+
+// toggle mechanism:
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = "40vh";
+    }
+  });
+}
